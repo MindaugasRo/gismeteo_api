61e33abb5522b0d8216285931d9e28928424a1a7
MindaugasRo <mindaugas.rocys@live.com>
2024-06-14 13:58:22 +0300

    Veikiantis duomenų išvedimas į ekraną

--- time.js
+++ time.js
@@ -5,7 +5,8 @@ async function searchCity(e) {
     let city = document.querySelector("#city");
     console.log(city.value);
     const data = await callApi(city.value);
-    printToConsole(data);
+    //printToConsole(data);
+    printToScreen(data)
     city.value = "";
 }
 
@@ -13,7 +14,7 @@ async function callApi(city) {
     let url = "https://api.meteo.lt/v1/places/" + city + "/forecasts/long-term";
         let response = await fetch(url);
         let data = await response.json();
-        console.log(data.forecastTimestamps);
+        // console.log(findCurrentHour(data.forecastTimestamps));
         return data;
 }
 
@@ -27,6 +28,39 @@ function findCurrentHour(forecasts) {
     );
   }
 
-function printToConsole(data) {
-    console.log(data.forecastTimestamps[0].forecastTimeUtc);
-}
+  function printToScreen(data) {
+    let weatherDataElement = document.getElementById("weather-data");
+   weatherDataElement.innerHTML = "";
+   
+          let forecastTimeUtc = findCurrentHour(data.forecastTimestamps).forecastTimeUtc;
+          let temperature = findCurrentHour(data.forecastTimestamps).feelsLikeTemperature;
+          let humidity = findCurrentHour(data.forecastTimestamps).relativeHumidity;
+          let condition = findCurrentHour(data.forecastTimestamps).conditionCode;
+  
+          weatherDataElement.innerHTML += `<p>Miestas: ${city.value}</p>`;
+          weatherDataElement.innerHTML += `<p>Laikas: ${forecastTimeUtc}</p>`;
+          weatherDataElement.innerHTML += `<p>Temperatūra: ${temperature}°C</p>`;
+          weatherDataElement.innerHTML += `<p>Drėgnumas: ${humidity}%</p>`;
+          weatherDataElement.innerHTML += `<p>Debesuotumas: ${condition}</p>`;
+    }
+  
+
+// function printToConsole(data) {
+//     const displayElement = document.getElementById('displayOutput');
+//     if (data) {
+//       const forecast = findCurrentHour(data.forecastTimestamps);
+//       displayElement.innerText = "Prognozė dabartinei valandai: " + forecast;
+//     } else {
+//       displayElement.innerHTML = "Dabartinės valandos prognozės nėra.";
+//     }
+//   }
+
+// jei duomenys negaunami išveda "Dabartinės valandos prognozės nėra"
+// function printToConsole(data) {
+//     const outputDiv = document.getElementById('displayOutput');
+//     if (data) {
+//       outputDiv.innerHTML = "Prognozė dabartinei valandai: " + findCurrentHour(data.forecastTimestamps);
+//     } else {
+//       outputDiv.innerHTML = "Dabartinės valandos prognozės nėra.";
+//     }
+//   }
